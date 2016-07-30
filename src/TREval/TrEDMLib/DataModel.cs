260eadec152a0f5e9e47586484b7748d28313fb9
Brent Samodien <brents@microsoft.com>
2016-07-30 13:09:17 -0700

    Some more Data Changes

---
+++ src/TREval/TrEDMLib/DataModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace TrEDMLib
+{
+    public class TrEvalData
+    {
+        public class TrEvalContext : DbContext
+        {
+            public DbSet<SessionEval> Evaluations { get; set; }
+        }
+
+        public class SessionEval
+        {
+            public int SessionEvalId { get; set; }
+            public string SessionName { get; set; }
+            public decimal SessionScore { get; set; }
+            public DateTime ScoreDate { get; set; }
+
+        }
+    }
+}
