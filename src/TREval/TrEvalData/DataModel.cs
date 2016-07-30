2a0adca4e9b4c25ab25b8a4578a6903275c121b0
Brent Samodien <brents@microsoft.com>
2016-07-30 10:00:42 -0700

    DataModel Added

---
+++ src/TREval/TrEvalData/DataModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrEvalData
+{
+    public class TrEvalContext : DbContext
+    {
+        public DbSet<SessionEval> Evaluations { get; set; }
+    }
+
+    public class SessionEval
+    {
+        public int SessionEvalId { get; set; }
+        public string SessionName { get; set; }
+        public decimal SessionScore { get; set; }
+        public DateTime ScoreDate { get; set; }
+
+    }
+}
